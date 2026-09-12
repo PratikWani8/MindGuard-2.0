e21dd4cd4698b9698d535c3c0f436a31dfc3ce57
Pratik Wani <pwani8306@gmail.com>
2026-09-12 14:11:45 +0530

    feat: add protected route component

---
+++ client/src/components/common/ProtectedRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import LoadingSpinner from "./LoadingSpinner";
+
+export default function ProtectedRoute() {
+  const { isAuthenticated, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <LoadingSpinner label="Checking your session" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location.pathname }} replace />;
+  }
+
+  return <Outlet />;
+}
